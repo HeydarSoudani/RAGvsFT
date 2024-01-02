18bc252be11949749640c63ea6bab62f6a18c372
HeydarSoudani <heydar.soudani@gmail.com>
2024-01-02 19:06:15 +0100

    running on server added

--- component1_retrieval/query_generation.py
+++ component1_retrieval/query_generation.py
@@ -1,28 +1,56 @@
+#!/usr/bin/env python3
+
+import logging, os
+import argparse
+import torch
+
 from beir import util, LoggingHandler
 from beir.datasets.data_loader import GenericDataLoader
 from beir.generation import QueryGenerator as QGen
 from beir.generation.models import QGenModel
 
-import logging
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
 
-data_path = "component1_retrieval/popqa_data"
-corpus = GenericDataLoader(data_path).load_corpus()
 
-model_path = "BeIR/query-gen-msmarco-t5-base-v1"
-generator = QGen(model=QGenModel(model_path))
+def main(args):
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0") 
+        print("Running on the GPU")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps:0")
+        print("Running on the mps")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
+    
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    
+    
+    data_path = "component1_retrieval/popqa_data"
+    corpus = GenericDataLoader(data_path).load_corpus()
+    
+    generator = QGen(model=QGenModel(args.model).to(device))
 
-prefix = "gen-3"
-ques_per_passage = 3
-batch_size = 64
+    prefix = "gen"
+    ques_per_passage = 3
+    batch_size = 64
 
-generator.generate(
-    corpus,
-    output_dir=data_path,
-    ques_per_passage=ques_per_passage,
-    prefix=prefix,
-    batch_size=batch_size)
+    generator.generate(
+        corpus,
+        output_dir=data_path,
+        ques_per_passage=ques_per_passage,
+        prefix=prefix,
+        batch_size=batch_size)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", required=True)
+    parser.add_argument("--output_dir", type=str, )
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/c1_query_generator_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=18
+#SBATCH --partition=gpu
+#SBATCH --time=1:10:00
+#SBATCH --output=script_logging/slurm_output_%A.out
+
+# Loading modules
+# module purge
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+# module load 2023
+# module load Python/3.11.3-GCCcore-12.3.0
+
+# srun python -uc "import torch; print('GPU available?', torch.cuda.is_available())"
+
+#Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
+pip install -r $HOME/RAGvsFT/component1_retrieval/requirements.txt
+
+srun $HOME/RAGvsFT/component1_retrieval/query_generation.py \
+    --model "BeIR/query-gen-msmarco-t5-large-v1" \
+    --output_dir "$HOME/RAGvsFT/component1_retrieval/output" \
+    
