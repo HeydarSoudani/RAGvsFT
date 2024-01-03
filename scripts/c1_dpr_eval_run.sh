7d42141272b8d1c31bf60869b326808321d10ff3
HeydarSoudani <heydar.soudani@gmail.com>
2024-01-03 08:56:03 +0100

    run eval

--- scripts/c1_dpr_eval_run.sh
+++ scripts/c1_dpr_eval_run.sh
@@ -4,8 +4,8 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=18
 #SBATCH --partition=gpu
-#SBATCH --time=1:10:00
-#SBATCH --output=script_logging/slurm_output_%A.out
+#SBATCH --time=0:30:00
+#SBATCH --output=script_logging/slurm_%A.out
 
 # Loading modules
 module load 2022
@@ -16,7 +16,7 @@ module load Python/3.10.4-GCCcore-11.3.0
 #Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
 pip install -r $HOME/RAGvsFT/component1_retrieval/requirements.txt
 
-srun $HOME/RAGvsFT/component1_retrieval/query_generation.py \
+srun $HOME/RAGvsFT/component1_retrieval/dpr/evaluation.py \
     --model "msmarco-distilbert-base-v3" \
     --output_results_dir "$HOME/RAGvsFT/component1_retrieval/results" \
     --output_results_filename "ft_dpr_beir.tsv" \
