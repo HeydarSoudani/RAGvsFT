cae88acd9630b52de98043a314b1b974d13a05f6
HeydarS <heydar.soudani@gmail.com>
2024-01-05 16:15:14 +0100

    clm v2

--- component4_CBQA/causalLM_ft.py
+++ component4_CBQA/causalLM_ft.py
@@ -4,10 +4,12 @@ import argparse, json, os, math
 import torch
 from datasets import Dataset, DatasetDict
 from sklearn.model_selection import train_test_split
+from accelerate import Accelerator
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import default_data_collator
 from transformers import TrainingArguments, Trainer, get_scheduler
-from accelerate import Accelerator
+
 
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
@@ -44,6 +46,12 @@ def main(args):
     else:
         device = torch.device("cpu")
         print("Running on the CPU")
+    
+    ### === Model introduction ============== 
+    if not os.path.exists(args.model_output_dir):
+        os.makedirs(args.model_output_dir)
+    model_save_path = os.path.join(args.model_output_dir, args.model_output_filename)
+    os.makedirs(model_save_path, exist_ok=True)
     
     model = AutoModelForCausalLM.from_pretrained(
         args.model,
@@ -86,33 +94,101 @@ def main(args):
         num_proc=4,
     )
     
+    ### ========================================
     ### === Training (v1) ======================
-    if not os.path.exists(args.model_output_dir):
-        os.makedirs(args.model_output_dir)
-    model_save_path = os.path.join(args.model_output_dir, args.model_output_filename)
-    os.makedirs(model_save_path, exist_ok=True)
     
-    training_args = TrainingArguments(
-        output_dir=model_save_path,
-        evaluation_strategy = "epoch",
-        num_train_epochs=args.epochs,
-        learning_rate=2e-5,
-        weight_decay=0.01,
-        # logging_dir='/content/drive/MyDrive/RAGvsFT/CLM_ft/logs',
-        # push_to_hub=True,
-    )
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=lm_datasets["train"],
-        eval_dataset=lm_datasets["validation"],
-    )
-    trainer.train()
+    # training_args = TrainingArguments(
+    #     output_dir=model_save_path,
+    #     evaluation_strategy = "epoch",
+    #     num_train_epochs=args.epochs,
+    #     learning_rate=2e-5,
+    #     weight_decay=0.01,
+    #     # logging_dir='/content/drive/MyDrive/RAGvsFT/CLM_ft/logs',
+    #     # push_to_hub=True,
+    # )
+    # trainer = Trainer(
+    #     model=model,
+    #     args=training_args,
+    #     train_dataset=lm_datasets["train"],
+    #     eval_dataset=lm_datasets["validation"],
+    # )
+    # trainer.train()
     
-    eval_results = trainer.evaluate()
-    print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+    # eval_results = trainer.evaluate()
+    # print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
     
+    ### ========================================
     ### === Training (v2) ======================
+    
+    # === Dataset
+    batch_size = 64
+    train_dataloader = DataLoader(
+        lm_datasets["train"],
+        shuffle=True,
+        batch_size=batch_size,
+        collate_fn=default_data_collator,
+    )
+    eval_dataloader = DataLoader(
+        lm_datasets["validation"],
+        batch_size=batch_size,
+        collate_fn=default_data_collator
+    )
+    
+    # === training parameters
+    optimizer = AdamW(model.parameters(), lr=2e-5)
+    accelerator = Accelerator()
+    model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+        model, optimizer, train_dataloader, eval_dataloader
+    )
+    num_update_steps_per_epoch = len(train_dataloader)
+    num_training_steps = args.epochs * num_update_steps_per_epoch
+    lr_scheduler = get_scheduler(
+        "linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
+    )
+    
+    # === training loop
+    progress_bar = tqdm(range(num_training_steps))
+    for epoch in range(args.epochs):
+        # Training
+        model.train()
+        for batch in train_dataloader:
+            outputs = model(**batch)
+            loss = outputs.loss
+            accelerator.backward(loss)
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+
+        # Evaluation
+        model.eval()
+        losses = []
+        for step, batch in enumerate(eval_dataloader):
+            with torch.no_grad():
+                outputs = model(**batch)
+
+            loss = outputs.loss
+            losses.append(accelerator.gather(loss.repeat(batch_size)))
+
+        losses = torch.cat(losses)
+        losses = losses[: len(lm_datasets["validation"])]
+        try:
+            perplexity = math.exp(torch.mean(losses))
+        except OverflowError:
+            perplexity = float("inf")
+
+        print(f">>> Epoch {epoch}: Perplexity: {perplexity}")
+        
+        # Save model
+        accelerator.wait_for_everyone()
+        unwrapped_model = accelerator.unwrap_model(model)
+        unwrapped_model.save_pretrained(model_save_path, save_function=accelerator.save)
+        if accelerator.is_main_process:
+            tokenizer.save_pretrained(model_save_path)
 
 
 if __name__ == "__main__":
