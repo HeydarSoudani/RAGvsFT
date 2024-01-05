537597fc24cc56e5f3648d1eb9dead50510076ee
HeydarS <heydar.soudani@gmail.com>
2024-01-05 17:30:42 +0100

    clm v3

--- component4_CBQA/causalLM_ft.py
+++ component4_CBQA/causalLM_ft.py
@@ -6,9 +6,11 @@ from datasets import Dataset, DatasetDict
 from sklearn.model_selection import train_test_split
 from accelerate import Accelerator
 
+from transformers import DataCollatorForLanguageModeling
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import default_data_collator
 from transformers import TrainingArguments, Trainer, get_scheduler
+from transformers import GPT2LMHeadModel, AutoConfig
 
 
 from torch.utils.data import DataLoader
@@ -47,6 +49,8 @@ def main(args):
         device = torch.device("cpu")
         print("Running on the CPU")
     
+    context_length = 128
+    
     ### === Model introduction ============== 
     if not os.path.exists(args.model_output_dir):
         os.makedirs(args.model_output_dir)
@@ -55,10 +59,10 @@ def main(args):
     
     model = AutoModelForCausalLM.from_pretrained(
         args.model,
-        load_in_8bit=True,
-        device_map='auto',
+        # load_in_8bit=True,
+        # device_map='auto',
     )
-    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
 
     ### === Load dataset =====================
     with open(args.corpus_path, 'r') as input_file:
@@ -74,25 +78,81 @@ def main(args):
     })
     print(dataset)
     
-    def tokenize_function(examples):
-        result = tokenizer(examples["text"])
-        if tokenizer.is_fast:
-            result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
-        return result
-    
-    tokenized_datasets = dataset.map(
-        tokenize_function,
+    def tokenize(element):
+        outputs = tokenizer(
+            element["text"],
+            truncation=True,
+            max_length=context_length,
+            return_overflowing_tokens=True,
+            return_length=True,
+        )
+        input_batch = []
+        for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+            if length == context_length:
+                input_batch.append(input_ids)
+        return {"input_ids": input_batch}
+
+    tokenized_dataset = dataset.map(
+        tokenize,
         batched=True,
-        num_proc=4,
-        remove_columns=["text"]
+        remove_columns=dataset["train"].column_names
     )
     
-    lm_datasets = tokenized_datasets.map(
-        group_texts,
-        batched=True,
-        batch_size=1000,
-        num_proc=4,
+    print(tokenized_dataset)
+    
+    config = AutoConfig.from_pretrained(
+        "gpt2",
+        vocab_size=len(tokenizer),
+        n_ctx=context_length,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
     )
+    
+    # model = GPT2LMHeadModel(config)
+    model_size = sum(t.numel() for t in model.parameters())
+    print(f"GPT-2 size: {model_size/1000**2:.1f}M parameters")
+    
+    
+
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    
+    args = TrainingArguments(
+        output_dir=model_save_path,
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
+        evaluation_strategy="steps",
+        eval_steps=5_000,
+        logging_steps=5_000,
+        gradient_accumulation_steps=8,
+        num_train_epochs=1,
+        weight_decay=0.1,
+        warmup_steps=1_000,
+        lr_scheduler_type="cosine",
+        learning_rate=5e-4,
+        save_steps=5_000,
+        # fp16=True,
+        # push_to_hub=True,
+    )
+
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=args,
+        data_collator=data_collator,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["validation"],
+    )
+    
+    trainer.train()
+
+    
+    # lm_datasets = tokenized_datasets.map(
+    #     group_texts,
+    #     batched=True,
+    #     batch_size=1000,
+    #     num_proc=4,
+    # )
     
     ### ========================================
     ### === Training (v1) ======================
@@ -121,72 +181,72 @@ def main(args):
     ### === Training (v2) ======================
     
     # === Dataset
-    batch_size = 64
-    train_dataloader = DataLoader(
-        lm_datasets["train"],
-        shuffle=True,
-        batch_size=batch_size,
-    )
-    eval_dataloader = DataLoader(
-        lm_datasets["validation"],
-        batch_size=batch_size
-    )
+    # batch_size = 64
+    # train_dataloader = DataLoader(
+    #     lm_datasets["train"],
+    #     shuffle=True,
+    #     batch_size=batch_size,
+    # )
+    # eval_dataloader = DataLoader(
+    #     lm_datasets["validation"],
+    #     batch_size=batch_size
+    # )
     
-    # === training parameters
-    optimizer = AdamW(model.parameters(), lr=2e-5)
-    accelerator = Accelerator()
-    model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
-        model, optimizer, train_dataloader, eval_dataloader
-    )
-    num_update_steps_per_epoch = len(train_dataloader)
-    num_training_steps = args.epochs * num_update_steps_per_epoch
-    lr_scheduler = get_scheduler(
-        "linear",
-        optimizer=optimizer,
-        num_warmup_steps=1000,
-        num_training_steps=num_training_steps,
-    )
+    # # === training parameters
+    # optimizer = AdamW(model.parameters(), lr=2e-5)
+    # accelerator = Accelerator()
+    # model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+    #     model, optimizer, train_dataloader, eval_dataloader
+    # )
+    # num_update_steps_per_epoch = len(train_dataloader)
+    # num_training_steps = args.epochs * num_update_steps_per_epoch
+    # lr_scheduler = get_scheduler(
+    #     "linear",
+    #     optimizer=optimizer,
+    #     num_warmup_steps=1000,
+    #     num_training_steps=num_training_steps,
+    # )
     
-    # === training loop
-    progress_bar = tqdm(range(num_training_steps))
-    for epoch in range(args.epochs):
-        # Training
-        model.train()
-        for batch in train_dataloader:
-            outputs = model(**batch)
-            loss = outputs.loss
-            accelerator.backward(loss)
+    # # === training loop
+    # progress_bar = tqdm(range(num_training_steps))
+    # for epoch in range(args.epochs):
+    #     # Training
+    #     model.train()
+    #     for batch in train_dataloader:
+    #         outputs = model(**batch)
+    #         loss = outputs.loss
+    #         accelerator.backward(loss)
 
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
-            progress_bar.update(1)
+    #         optimizer.step()
+    #         lr_scheduler.step()
+    #         optimizer.zero_grad()
+    #         progress_bar.update(1)
 
-        # Evaluation
-        model.eval()
-        losses = []
-        for step, batch in enumerate(eval_dataloader):
-            with torch.no_grad():
-                outputs = model(**batch)
+    #     # Evaluation
+    #     model.eval()
+    #     losses = []
+    #     for step, batch in enumerate(eval_dataloader):
+    #         with torch.no_grad():
+    #             outputs = model(**batch)
 
-            loss = outputs.loss
-            losses.append(accelerator.gather(loss.repeat(batch_size)))
+    #         loss = outputs.loss
+    #         losses.append(accelerator.gather(loss.repeat(batch_size)))
 
-        losses = torch.cat(losses)
-        losses = losses[: len(lm_datasets["validation"])]
-        try:
-            perplexity = math.exp(torch.mean(losses))
-        except OverflowError:
-            perplexity = float("inf")
+    #     losses = torch.cat(losses)
+    #     losses = losses[: len(lm_datasets["validation"])]
+    #     try:
+    #         perplexity = math.exp(torch.mean(losses))
+    #     except OverflowError:
+    #         perplexity = float("inf")
 
-        print(f">>> Epoch {epoch}: Perplexity: {perplexity}")
+    #     print(f">>> Epoch {epoch}: Perplexity: {perplexity}")
         
-        # Save model
-        accelerator.wait_for_everyone()
-        unwrapped_model = accelerator.unwrap_model(model)
-        unwrapped_model.save_pretrained(model_save_path, save_function=accelerator.save)
-        if accelerator.is_main_process:
-            tokenizer.save_pretrained(model_save_path)
+    #     # Save model
+    #     accelerator.wait_for_everyone()
+    #     unwrapped_model = accelerator.unwrap_model(model)
+    #     unwrapped_model.save_pretrained(model_save_path, save_function=accelerator.save)
+    #     if accelerator.is_main_process:
+    #         tokenizer.save_pretrained(model_save_path)
 
 
 if __name__ == "__main__":
