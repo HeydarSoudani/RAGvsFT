c4e8ef5e2ac6c3ff3bd9c5c529bb0a57c02bf787
HeydarS <heydar.soudani@gmail.com>
2024-01-07 17:48:52 +0100

    get wo bk results

--- component1_retrieval/dpr/evaluation.py
+++ component1_retrieval/dpr/evaluation.py
@@ -52,9 +52,14 @@ def main(args):
         device = torch.device("cpu")
         print("Running on the CPU")
     
-    # if not os.path.exists(args.output_results_dir):
-    #     os.makedirs(args.output_results_dir)
-    # resutls_path = os.path.join(args.output_results_dir, args.output_results_filename)
+    queries_bk_path = "component0_preprocessing/generated_data/popQA_costomized/queries_bucketing.json"
+    
+    if not os.path.exists(args.output_results_dir):
+        os.makedirs(args.output_results_dir)
+    resutls_wbk_path = os.path.join(args.output_results_dir, 'wbk_'+args.output_results_filename)
+    resutls_wobk_path = os.path.join(args.output_results_dir, 'wobk_'+args.output_results_filename)
+    
+    
     
     model = DRES(models.SentenceBERT(args.model, batch_size=128), device=device)
     # model = DRES(models.SentenceBERT((
@@ -65,82 +70,105 @@ def main(args):
     
     dataloader = CostomizedGenericDataLoader(data_folder=args.data_path)
     corpus, queries = dataloader.load_corpus_queries()
-    
     results = retriever.retrieve(corpus, queries)
     
     # save qrels file
-    # if args.results_save_file:
-    #     qrels_resutls_path = os.path.join(args.output_results_dir, args.results_save_file)
-    #     k = 1
-    #     with open(qrels_resutls_path, 'w', newline='') as file:
-    #         writer = csv.DictWriter(file, fieldnames=['query_id', 'corpus_id', 'score'], delimiter='\t')
-    #         writer.writeheader()
-    #         for query_id, value in results.items():
-    #             sorted_doc_scores = dict(sorted(value.items(), key=lambda item: item[1], reverse=True))
-    #             writer.writerow({
-    #                 'query_id': query_id,
-    #                 'corpus_id': list(sorted_doc_scores.keys())[0],
-    #                 'score': list(sorted_doc_scores.values())[0]
-    #             })
+    if args.results_save_file:
+        qrels_resutls_path = os.path.join(args.output_results_dir, args.results_save_file)
+        k = 1
+        with open(qrels_resutls_path, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['query_id', 'corpus_id', 'score'], delimiter='\t')
+            writer.writeheader()
+            for query_id, value in results.items():
+                sorted_doc_scores = dict(sorted(value.items(), key=lambda item: item[1], reverse=True))
+                writer.writerow({
+                    'query_id': query_id,
+                    'corpus_id': list(sorted_doc_scores.keys())[0],
+                    'score': list(sorted_doc_scores.values())[0]
+                })
     
-    # Without bucketting results
-    qrels = preprocessing_qrels([])                    
-    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
-
-    logging.info('without bucketting')
-    logging.info(ndcg)
-    logging.info(_map)
-    logging.info(recall)
-    logging.info(precision)
-    print("\n")
+    with open(queries_bk_path, 'r') as in_queries:
+        query_data = json.load(in_queries)
+    
+    
+    # Without bucketting
+    with open(resutls_wobk_path, 'w', newline='') as file:
+        tsv_writer = csv.writer(file, delimiter='\t')
+        tsv_writer.writerow([
+            "Title",
+            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
+            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
+            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
+            "P@1", "P@5", "P@10", "P@100",
+        ])
+        
+        for relation_name, relation_data in query_data.items():    
+            rel_data = []
+            for bk_name, bk_data in relation_data.items():
+                rel_data.extend(bk_data)
+            
+            qid_list = [q_sample["entity_id"] for q_sample in rel_data]
+            qrels = preprocessing_qrels(qid_list)
+            ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
+            logging.info(ndcg)
+            logging.info(_map)
+            logging.info(recall)
+            logging.info(precision)
+            print(ndcg)
+            print(_map)
+            print(recall)
+            print(precision)
+            print("\n")
+        
+            eval_res = [relation_name]\
+                +list(ndcg.values())\
+                +list(_map.values())\
+                +list(recall.values())\
+                +list(precision.values())
+            tsv_writer.writerow(eval_res)
         
     
-    # # With bucketting results
-    # with open(resutls_path, 'w', newline='') as file:
-    #     tsv_writer = csv.writer(file, delimiter='\t')
-    #     tsv_writer.writerow([
-    #         "Title",
-    #         "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
-    #         "MAP@1", "MAP@5", "MAP@10", "MAP@100",
-    #         "Recall@1", "Recall@5", "Recall@10", "Recall@100",
-    #         "P@1", "P@5", "P@10", "P@100",
-    #     ])
+    # With bucketting
+    with open(resutls_wbk_path, 'w', newline='') as file:
+        tsv_writer = csv.writer(file, delimiter='\t')
+        tsv_writer.writerow([
+            "Title",
+            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
+            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
+            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
+            "P@1", "P@5", "P@10", "P@100",
+        ])
         
-        
-    #     queries_bk_path = "component0_preprocessing/generated_data/popQA_costomized/queries_bucketing.json"
-    #     with open(queries_bk_path, 'r') as in_queries:
-    #         query_data = json.load(in_queries)
-        
-    #     for relation_name, relation_data in query_data.items():
-    #         for bk_name, bk_data in relation_data.items():
-    #             # print('{}, {}'.format(relation_name, bk_name))
-    #             logging.info('{}, {}'.format(relation_name, bk_name))
+        for relation_name, relation_data in query_data.items():
+            for bk_name, bk_data in relation_data.items():
+                # print('{}, {}'.format(relation_name, bk_name))
+                logging.info('{}, {}'.format(relation_name, bk_name))
                 
-    #             if len(bk_data) == 0:
-    #                 eval_res = [relation_name+'_'+bk_name] + [0]*16
+                if len(bk_data) == 0:
+                    eval_res = [relation_name+'_'+bk_name] + [0]*16
 
-    #             else:
-    #                 qid_list = [q_sample["entity_id"] for q_sample in bk_data]
-    #                 qrels = preprocessing_qrels(qid_list)
+                else:
+                    qid_list = [q_sample["entity_id"] for q_sample in bk_data]
+                    qrels = preprocessing_qrels(qid_list)
                     
-    #                 ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
+                    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
 
-    #                 logging.info(ndcg)
-    #                 logging.info(_map)
-    #                 logging.info(recall)
-    #                 logging.info(precision)
-    #                 # print(ndcg)
-    #                 # print(_map)
-    #                 # print(recall)
-    #                 # print(precision)
-    #                 print("\n")
+                    logging.info(ndcg)
+                    logging.info(_map)
+                    logging.info(recall)
+                    logging.info(precision)
+                    # print(ndcg)
+                    # print(_map)
+                    # print(recall)
+                    # print(precision)
+                    print("\n")
         
-    #                 eval_res = [relation_name+'_'+bk_name]\
-    #                     +list(ndcg.values())\
-    #                     +list(_map.values())\
-    #                     +list(recall.values())\
-    #                     +list(precision.values())
-    #             tsv_writer.writerow(eval_res)
+                    eval_res = [relation_name+'_'+bk_name]\
+                        +list(ndcg.values())\
+                        +list(_map.values())\
+                        +list(recall.values())\
+                        +list(precision.values())
+                tsv_writer.writerow(eval_res)
     
 
 if __name__ == "__main__":
