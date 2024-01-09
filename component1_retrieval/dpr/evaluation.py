79a016d8588aaf75bd9e2918f9d58ab399a1f913
HeydarS <heydar.soudani@gmail.com>
2024-01-09 12:34:11 +0100

    rerank added

--- component1_retrieval/dpr/evaluation.py
+++ component1_retrieval/dpr/evaluation.py
@@ -2,46 +2,22 @@
 
 import torch
 import argparse
-import csv, os, json
 import logging
 
-from beir import util, LoggingHandler
+from beir import LoggingHandler
 from beir.retrieval import models
-from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
 
-from customized_datasets.data_loader import CostomizedGenericDataLoader
+from component1_retrieval.customized_datasets.data_loader import CostomizedGenericDataLoader
+from component1_retrieval.utils import save_qrels_file, save_evaluation_files
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
 
-def preprocessing_qrels(qid_list):
-    input_qrels = "component0_preprocessing/generated_data/popQA_costomized/qrels.jsonl"
-    
-    with open(input_qrels, 'r') as in_qrels: 
-        qrels = {}
-        for line in in_qrels:
-            data = json.loads(line)
-            query_id, corpus_id, score = data["query_id"], data["doc_id"], int(data["score"])
-            
-            if len(qid_list) == 0:
-                if query_id not in qrels:
-                    qrels[query_id] = {corpus_id: score}
-                else:
-                    qrels[query_id][corpus_id] = score
-            else:
-                if data["query_id"] in qid_list:
-                    if query_id not in qrels:
-                        qrels[query_id] = {corpus_id: score}
-                    else:
-                        qrels[query_id][corpus_id] = score
-        return qrels
-
 def main(args):
-    
     if torch.cuda.is_available():
         device = torch.device("cuda:0") 
         print("Running on the GPU")
@@ -51,14 +27,6 @@ def main(args):
     else:
         device = torch.device("cpu")
         print("Running on the CPU")
-    
-    queries_bk_path = "component0_preprocessing/generated_data/popQA_costomized/queries_bucketing.json"
-    
-    if not os.path.exists(args.output_results_dir):
-        os.makedirs(args.output_results_dir)
-    resutls_wbk_path = os.path.join(args.output_results_dir, 'wbk_'+args.output_results_filename)
-    resutls_wobk_path = os.path.join(args.output_results_dir, 'wobk_'+args.output_results_filename)
-    
     
     model = DRES(models.SentenceBERT(args.model, batch_size=128), device=device)
     # model = DRES(models.SentenceBERT((
@@ -71,103 +39,8 @@ def main(args):
     corpus, queries = dataloader.load_corpus_queries()
     results = retriever.retrieve(corpus, queries)
     
-    # save qrels file
-    if args.results_save_file:
-        qrels_resutls_path = os.path.join(args.output_results_dir, args.results_save_file)
-        k = 1
-        with open(qrels_resutls_path, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['query_id', 'corpus_id', 'score'], delimiter='\t')
-            writer.writeheader()
-            for query_id, value in results.items():
-                sorted_doc_scores = dict(sorted(value.items(), key=lambda item: item[1], reverse=True))
-                writer.writerow({
-                    'query_id': query_id,
-                    'corpus_id': list(sorted_doc_scores.keys())[0],
-                    'score': list(sorted_doc_scores.values())[0]
-                })
-    
-    with open(queries_bk_path, 'r') as in_queries:
-        query_data = json.load(in_queries)
-    
-    # Without bucketting
-    with open(resutls_wobk_path, 'w', newline='') as file:
-        tsv_writer = csv.writer(file, delimiter='\t')
-        tsv_writer.writerow([
-            "Title",
-            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
-            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
-            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
-            "P@1", "P@5", "P@10", "P@100",
-        ])
-        
-        for relation_name, relation_data in query_data.items():    
-            rel_data = []
-            for bk_name, bk_data in relation_data.items():
-                rel_data.extend(bk_data)
-            
-            qid_list = [q_sample["entity_id"] for q_sample in rel_data]
-            qrels = preprocessing_qrels(qid_list)
-            ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
-            logging.info(ndcg)
-            logging.info(_map)
-            logging.info(recall)
-            logging.info(precision)
-            print(ndcg)
-            print(_map)
-            print(recall)
-            print(precision)
-            print("\n")
-        
-            eval_res = [relation_name]\
-                +list(ndcg.values())\
-                +list(_map.values())\
-                +list(recall.values())\
-                +list(precision.values())
-            tsv_writer.writerow(eval_res)
-        
-    
-    # With bucketing
-    with open(resutls_wbk_path, 'w', newline='') as file:
-        tsv_writer = csv.writer(file, delimiter='\t')
-        tsv_writer.writerow([
-            "Title",
-            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
-            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
-            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
-            "P@1", "P@5", "P@10", "P@100",
-        ])
-        
-        for relation_name, relation_data in query_data.items():
-            for bk_name, bk_data in relation_data.items():
-                # print('{}, {}'.format(relation_name, bk_name))
-                logging.info('{}, {}'.format(relation_name, bk_name))
-                
-                if len(bk_data) == 0:
-                    eval_res = [relation_name+'_'+bk_name] + [0]*16
-
-                else:
-                    qid_list = [q_sample["entity_id"] for q_sample in bk_data]
-                    qrels = preprocessing_qrels(qid_list)
-                    
-                    ndcg, _map, recall, precision = retriever.evaluate(qrels, results, [1, 5, 10, 100]) #retriever.k_values
-
-                    logging.info(ndcg)
-                    logging.info(_map)
-                    logging.info(recall)
-                    logging.info(precision)
-                    # print(ndcg)
-                    # print(_map)
-                    # print(recall)
-                    # print(precision)
-                    print("\n")
-        
-                    eval_res = [relation_name+'_'+bk_name]\
-                        +list(ndcg.values())\
-                        +list(_map.values())\
-                        +list(recall.values())\
-                        +list(precision.values())
-                tsv_writer.writerow(eval_res)
-    
+    save_qrels_file(results, args)
+    save_evaluation_files(retriever, results, args)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
---
+++ component1_retrieval/rerank/evaluation.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import torch
+import argparse
+import json
+import logging
+
+from beir import LoggingHandler
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.lexical import BM25Search as BM25
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+from beir.retrieval import models
+
+from component1_retrieval.customized_datasets.data_loader import CostomizedGenericDataLoader
+from component1_retrieval.utils import save_qrels_file, save_evaluation_files
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+def main(args):
+    
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0") 
+        print("Running on the GPU")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps:0")
+        print("Running on the mps")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
+    
+    dataloader = CostomizedGenericDataLoader(data_folder=args.data_path)
+    corpus, queries = dataloader.load_corpus_queries()
+    
+    # === First stage with Elastic-search ===========
+    # hostname = "http://localhost:9200"
+    # index_name = "popqa"
+    # initialize = True
+    # model = BM25(index_name=index_name, hostname=hostname, initialize=initialize)
+    # retriever = EvaluateRetrieval(model)
+    # results = retriever.retrieve(corpus, queries)
+    
+    # === First stage from Google Colab =============
+    bm25_results_path = 'component1_retrieval/rerank/data/bm25_results_rerank.json'
+    with open(bm25_results_path, 'r') as f:
+        results = json.load(f)
+    
+    #### Reranking top-100 docs using Dense Retriever model 
+    model = DRES(models.SentenceBERT(args.dense_model), batch_size=128, device=device)
+    dense_retriever = EvaluateRetrieval(model, score_function="dot", k_values=[1,5,10,100])
+    print('a')
+    rerank_results = dense_retriever.rerank(corpus, queries, results, top_k=100)
+    print('b')
+    
+    save_qrels_file(rerank_results, args)
+    save_evaluation_files(dense_retriever, rerank_results, args)
+    # ndcg, _map, recall, precision, hole = dense_retriever.evaluate(qrels, rerank_results, retriever.k_values)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dense_model", required=True)
+    parser.add_argument("--data_path", type=str)
+    parser.add_argument("--output_results_dir", type=str)
+    parser.add_argument("--output_results_filename", type=str)
+    parser.add_argument("--results_save_file", default=None, type=str)
+    args = parser.parse_args()
+    main(args)
