d39ffb183af7c92ff017393fc27b824fd86e4745
HeydarSoudani <heydar.soudani@gmail.com>
2024-01-10 17:31:57 +0100

    some running changes

--- scripts/c3_pipeline_QAG.sh
+++ scripts/c3_pipeline_QAG.sh
@@ -8,19 +8,21 @@
 #SBATCH --output=script_logging/slurm_%A.out
 
 # Loading modules
-# module load 2022
-# module load Python/3.10.4-GCCcore-11.3.0
-module load 2023
-module load Python/3.11.3-GCCcore-12.3.0
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+# module load 2023
+# module load Python/3.11.3-GCCcore-12.3.0
 
 # srun python -uc "import torch; print('GPU available?', torch.cuda.is_available())"
 
 #Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
-pip install -r $HOME/RAGvsFT/component4_CBQA/requirements.txt
+pip install -r $HOME/RAGvsFT/component3_QAGeneration/requirements.txt
+pip install --upgrade pip
+python -m spacy download en_core_web_sm
 
 srun $HOME/RAGvsFT/component3_QAGeneration/pipeline.py \
-    --qg_model 'lmqg/t5-small-squad-qg' \
-    --ae_model 'lmqg/t5-small-squad-ae' \
+    --qg_model 'lmqg/t5-large-squad-qg' \
+    --ae_model 'lmqg/t5-large-squad-ae' \
     --corpus_path "$HOME/RAGvsFT/component0_preprocessing/generated_data/popQA_costomized/corpus.jsonl" \
     --results_output_dir "$HOME/RAGvsFT/component3_QAGeneration/generated_data" \
     --results_output_filename "qag_results.jsonl"
