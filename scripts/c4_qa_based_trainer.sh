7b1f061dae4561dd5788bf9cfe3a38909f06eeb9
HeydarS <heydar.soudani@gmail.com>
2024-01-12 12:40:26 +0100

    corpus ready files new

--- scripts/c4_qa_based_trainer.sh
+++ scripts/c4_qa_based_trainer.sh
@@ -17,7 +17,7 @@ module load Python/3.10.4-GCCcore-11.3.0
 pip install -r $HOME/RAGvsFT/component4_CBQA/requirements.txt
 
 srun $HOME/RAGvsFT/component4_CBQA/qa_based_trainer.py \
-    --model_name_or_path "facebook/opt-350m" \
+    --model_name_or_path "facebook/opt-125m" \
     --data_path "$HOME/RAGvsFT/component3_QAGeneration/generated_data/qag_results.jsonl" \
     --model_output_dir "$HOME/RAGvsFT/component4_CBQA/models" \
     --model_output_filename "qa_opt350m_2e" \
@@ -25,5 +25,7 @@ srun $HOME/RAGvsFT/component4_CBQA/qa_based_trainer.py \
     --batch_size 2 \
     --with_peft
 
+# "facebook/opt-125m"
+# "facebook/opt-350m"
 # "facebook/opt-1.3b"
 # "$HOME/RAGvsFT/component4_CBQA/models/clm_opt1-3b_1e"
