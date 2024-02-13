5b64ba363ac951db9266a5c0fb1265633c254ceb
HeydarSoudani <heydar.soudani@gmail.com>
2024-02-13 08:54:36 +0100

    some more files

--- component4_CBQA/flant5_evaluation.py
+++ component4_CBQA/flant5_evaluation.py
@@ -169,7 +169,7 @@ def load_model(args):
     return model, tokenizer
 
 def main(args):
-    pre_prefix = "af"
+    pre_prefix = "af_extra"
     rag_part = "rag" if args.with_rag else "norag"
     peft_part = "peft" if args.with_peft else "full"
     if args.with_rag:
