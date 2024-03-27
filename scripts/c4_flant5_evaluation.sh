31f0fd15f52b30b8c3cccbe29b021cacbdb41907
HeydarSoudani <heydar.soudani@gmail.com>
2024-03-27 10:18:02 +0100

    some res files

--- scripts/c4_flant5_evaluation.sh
+++ scripts/c4_flant5_evaluation.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=18
 #SBATCH --partition=gpu
-#SBATCH --time=5:00:00
+#SBATCH --time=3:00:00
 #SBATCH --output=script_logging/slurm_%A.out
 
 # Loading modules
@@ -17,14 +17,14 @@ module load Python/3.10.4-GCCcore-11.3.0
 # pip install -r $HOME/RAGvsFT/component4_CBQA/requirements.txt
 
 srun $HOME/RAGvsFT/component4_CBQA/flant5_evaluation.py \
-    --model_name_or_path $HOME/RAGvsFT/component4_CBQA/models/flan-t5-large_peft_v26/checkpoint-1491 \
+    --model_name_or_path "google/flan-t5-large" \
     --data_dir $HOME/RAGvsFT/component0_preprocessing/generated_data/popQA_EQformat \
     --output_result_dir $HOME/RAGvsFT/component0_preprocessing/generated_data/popQA_EQformat \
-    --output_file_pre_prefix "af_e3" \
-    --with_peft True \
+    --output_file_pre_prefix "bf" \
+    --with_peft False \
     --with_fs False \
-    --with_rag False \
-    --retrieval_method ""
+    --with_rag True \
+    --retrieval_method "bm25"
 
 # ['ideal', 'dpr', 'contriever', 'rerank', 'bm25']
 # "google/flan-t5-xxl"
