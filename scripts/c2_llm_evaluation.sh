837151855e10542376ad8efb166ab5fca4cd1064
HeydarSoudani <heydar.soudani@gmail.com>
2024-04-08 19:59:03 +0200

    files

--- scripts/c2_llm_evaluation.sh
+++ scripts/c2_llm_evaluation.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=18
 #SBATCH --partition=gpu
-#SBATCH --time=20:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=script_logging/slurm_%A.out
 
 # Loading modules
@@ -12,22 +12,24 @@ module load 2022
 module load Python/3.10.4-GCCcore-11.3.0
 
 srun $HOME/RAGvsFT/component2_AnsGen/evaluation/llm_evaluation.py \
-    --model_name_or_path "google/flan-t5-xxl" \
+    --model_name_or_path "google/flan-t5-small" \
     --llm_model_name "flant5" \
-    --dataset_name "EQ" \
-    --output_file_pre_prefix "xxl_bf" \
+    --dataset_name "popQA" \
+    --output_file_pre_prefix "sm_bf" \
     --with_peft False \
-    --with_rag True \
-    --retrieval_method "ideal"
+    --with_rag False \
+    --retrieval_method ""
 
 
 # output_file_pre_prefix -> 
     # - For Flan: []_bf
 # Model name: [
     # flant5: "google/flan-t5-xxl" [small, base, large, xl, xxl]
+    
     # tiny_llama: "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
     # stable_lm2: "stabilityai/stablelm-2-zephyr-1_6b"
     # MiniCPM: "openbmb/MiniCPM-2B-sft-fp32"
+    
     # llama2: "meta-llama/Llama-2-7b-chat-hf"
     # mistral: "mistralai/Mistral-7B-Instruct-v0.1"
     # zephyr: "HuggingFaceH4/zephyr-7b-beta"
