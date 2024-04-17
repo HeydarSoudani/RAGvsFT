26854f1b25170958ad6f5f6e59179d5471742c9a
HeydarS <heydar.soudani@gmail.com>
2024-04-17 09:05:45 +0200

    bm25 added

--- component1_retrieval/BM25.py
+++ component1_retrieval/BM25.py
@@ -4,186 +4,63 @@
 # pip install faiss-cpu -q
 # pip install beir -q
 
+import json, os
+import logging
+import argparse
+import torch
+
 from beir import LoggingHandler
+from beir.retrieval.search.lexical import BM25Search as BM25
 from beir.retrieval.evaluation import EvaluateRetrieval
 from pyserini.search.lucene import LuceneSearcher
-import json, os, csv
-import logging
+
+from component1_retrieval.customized_datasets.data_loader import CostomizedGenericDataLoader
+from component1_retrieval.utils import save_qrels_file, save_evaluation_files_v1, save_evaluation_files_v2
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
 
-def load_queries(file_path):
-    queries = {}
-    with open(file_path, 'r') as file:
-        for line in file:
-            data = json.loads(line)
-            queries[data['entity_id']] = data['question']
-    return queries
-
-def preprocessing_qrels(qrels_file, qid_list):
-    with open(qrels_file, 'r') as in_qrels: 
-        qrels = {}
-        for line in in_qrels:
-            data = json.loads(line)
-            query_id, corpus_id, score = data["query_id"], data["doc_id"], int(data["score"])
-            
-            if len(qid_list) == 0:
-                if query_id not in qrels:
-                    qrels[query_id] = {corpus_id: score}
-                else:
-                    qrels[query_id][corpus_id] = score
-            else:
-                if data["query_id"] in qid_list:
-                    if query_id not in qrels:
-                        qrels[query_id] = {corpus_id: score}
-                    else:
-                        qrels[query_id][corpus_id] = score
-        return qrels
-
-def retrieve(corpus_pyserini_index_path, queries, k, results_save_file=None):
-    searcher = LuceneSearcher(corpus_pyserini_index_path)
-    results = {}
+def main(args):
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0") 
+        print("Running on the GPU")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps:0")
+        print("Running on the mps")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
     
-    for query_id, query_text in queries.items():
-        hits = searcher.search(query_text, k)
-        
-        for hit in hits:
-            if query_id not in results:
-                results[query_id] = {hit.docid: hit.score}
-            else:
-                results[query_id][hit.docid] = hit.score
-
-    if results_save_file:
-        with open(results_save_file, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['query_id', 'corpus_id', 'score'], delimiter='\t')
-            writer.writeheader()
-            for query_id, value in results.items():
-                writer.writerow({
-                    'query_id': query_id,
-                    'corpus_id': list(value.keys())[0],
-                    'score': list(value.values())[0]
-                })
-    return results
-
-def main():
-    # Files for local
-    # corpus_file = 'data/generated/popQA_costomized/corpus.jsonl'
-    # queries_file = 'data/generated/popQA_costomized/queries.jsonl'
-    # qrels_file = 'data/generated/popQA_costomized/qrels.jsonl'
-    # queries_bk_path = "data/generated/popQA_costomized/queries_bucketing.json"
-    # output_results_dir = "component1_retrieval/results"
-    # output_results_filename = "bm25_eval.tsv"
-
-    # Files for Google drive
-    corpus_file = '/content/drive/MyDrive/RAGvsFT/data_bm25/corpus.jsonl'
-    queries_file = '/content/drive/MyDrive/RAGvsFT/data_bm25/queries.jsonl'
-    qrels_file = '/content/drive/MyDrive/RAGvsFT/data_bm25/qrels.jsonl'
-    queries_bk_path = "/content/drive/MyDrive/RAGvsFT/data_bm25/queries_bucketing.json"
-    corpus_pyserini_index_path = '/content/drive/MyDrive/RAGvsFT/data_bm25/index'
-
-    output_results_dir = "/content/drive/MyDrive/RAGvsFT/data_bm25"
-    output_results_filename = "bm25_eval.tsv"
-    results_save_file = 'bm25-qrels.tsv'
-
-    top_k = 100
-    queries = load_queries(queries_file)
-
-    if not os.path.exists(output_results_dir):
-        os.makedirs(output_results_dir)
-    resutls_wbk_path = os.path.join(output_results_dir, 'wbk_'+output_results_filename)
-    resutls_wobk_path = os.path.join(output_results_dir, 'wobk_'+output_results_filename)
+    dataloader = CostomizedGenericDataLoader(data_folder = args.data_path)
+    corpus, queries = dataloader.load_corpus_queries()
     
-    # retrieve and save in qrels file
-    qrels_resutls_path = os.path.join(output_results_dir, results_save_file)
-    ret_results = retrieve(corpus_pyserini_index_path, queries, top_k, qrels_resutls_path)
     
-    ret_evaluator = EvaluateRetrieval()
-
-    with open(queries_bk_path, 'r') as in_queries:
-        query_data = json.load(in_queries)
-
-    # Without bucketting
-    with open(resutls_wobk_path, 'w', newline='') as file:
-        tsv_writer = csv.writer(file, delimiter='\t')
-        tsv_writer.writerow([
-            "Title",
-            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
-            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
-            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
-            "P@1", "P@5", "P@10", "P@100",
-        ])
-
-        for relation_name, relation_data in query_data.items():
-            
-            rel_data = []
-            for bk_name, bk_data in relation_data.items():
-                rel_data.extend(bk_data)
-            
-            qid_list = [q_sample["entity_id"] for q_sample in rel_data]
-            qrels = preprocessing_qrels(qrels_file, qid_list)
-            ndcg, _map, recall, precision = ret_evaluator.evaluate(qrels, ret_results, [1, 5, 10, 100]) #retriever.k_values
-            logging.info(ndcg)
-            logging.info(_map)
-            logging.info(recall)
-            logging.info(precision)
-            print(ndcg)
-            print(_map)
-            print(recall)
-            print(precision)
-            print("\n")
-        
-            eval_res = [relation_name]\
-                +list(ndcg.values())\
-                +list(_map.values())\
-                +list(recall.values())\
-                +list(precision.values())
-            tsv_writer.writerow(eval_res)
-
-    # With bucketting
-    with open(resutls_wbk_path, 'w', newline='') as file:
-        tsv_writer = csv.writer(file, delimiter='\t')
-        tsv_writer.writerow([
-            "Title",
-            "NDCG@1", "NDCG@5", "NDCG@10", "NDCG@100",
-            "MAP@1", "MAP@5", "MAP@10", "MAP@100",
-            "Recall@1", "Recall@5", "Recall@10", "Recall@100",
-            "P@1", "P@5", "P@10", "P@100",
-        ])
-        for relation_name, relation_data in query_data.items():
-            for bk_name, bk_data in relation_data.items():
-                print('{}, {}'.format(relation_name, bk_name))
-                logging.info('{}, {}'.format(relation_name, bk_name))
-
-                if len(bk_data) == 0:
-                    eval_res = [relation_name+'_'+bk_name] + [0]*16
-
-                else:
-                    qid_list = [q_sample["entity_id"] for q_sample in bk_data]
-                    qrels = preprocessing_qrels(qrels_file, qid_list)
-
-                    ndcg, _map, recall, precision = ret_evaluator.evaluate(qrels, ret_results, [1, 5, 10, 100]) #retriever.k_values
-
-                    logging.info(ndcg)
-                    logging.info(_map)
-                    logging.info(recall)
-                    logging.info(precision)
-                    print(ndcg)
-                    print(_map)
-                    print(recall)
-                    print(precision)
-                    print("\n")
-
-                    eval_res = [relation_name+'_'+bk_name]\
-                        +list(ndcg.values())\
-                        +list(_map.values())\
-                        +list(recall.values())\
-                        +list(precision.values())
-                tsv_writer.writerow(eval_res)
+    hostname = "localhost" 
+    index_name = f"{args.dataset_name}_index" 
+    initialize = True 
+    
+    model = BM25(index_name=index_name, hostname=hostname, initialize=initialize)
+    retriever = EvaluateRetrieval(model)
+    results = retriever.retrieve(corpus, queries)
+    
+    if not os.path.exists(args.output_results_dir):
+        os.makedirs(args.output_results_dir)
+    
+    save_qrels_file(results, args)
+    # save_evaluation_files_v1(retriever, results, args)
+    save_evaluation_files_v2(retriever, results, args)
+    
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str, required=True)
+    parser.add_argument("--data_path", type=str)
+    parser.add_argument("--output_results_dir", type=str)
+    parser.add_argument("--output_results_filename", type=str)
+    parser.add_argument("--results_save_file", default=None, type=str)
+    args = parser.parse_args()
+    main(args)
     
     
