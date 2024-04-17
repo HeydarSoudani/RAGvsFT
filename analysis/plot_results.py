6046bcde83249fa8bbbd4390e854470c8d01e16d
HeydarS <heydar.soudani@gmail.com>
2024-04-18 00:37:41 +0200

    files changes

--- analysis/plot_results.py
+++ analysis/plot_results.py
@@ -7,7 +7,7 @@ import json
 import os
 
 # === Datasets variables ========================
-dataset_name = 'EQ' # [popQA, witQA, EQ]
+dataset_name = 'popQA' # [popQA, witQA, EQ]
 retrieval_models = ["bm25", "contriever", "rerank", "dpr"]
 gen_models = [
     "flant5_sm", "flant5_bs", "flant5_lg", "flant5_xl", "flant5_xxl",
@@ -403,7 +403,7 @@ def calculated_accuracy(objects):
 def plot_answer_generator_results(per_relation=False, per_bucket=False, only_all=False):
     
     ### ==== Define Variables =============
-    model_name = gen_models[7]
+    model_name = gen_models[1]
     
     if model_name in ["flant5_sm", "flant5_bs", "flant5_lg", "flant5_xl", "flant5_xxl"]:
         model_type = 'flant5'
@@ -421,7 +421,8 @@ def plot_answer_generator_results(per_relation=False, per_bucket=False, only_all
         {"title": f"FT/idealRAG", "filename": f"{base_path}/{dataset_name}_costomized/results/{model_type}/{dataset_name}_{model_name}_af_rag_{retrieval_model}_peft_results.jsonl"},
         # {"title": f"voting", "filename": f"{base_path}/{dataset_name}_costomized/results/{model_type}/{dataset_name}_{model_name}_voting_results.jsonl"},
         # {"title": f"voting_2", "filename": f"{base_path}/{dataset_name}_costomized/results/{model_type}/{dataset_name}_{model_name}_voting_2_results.jsonl"},
-        # {"title": f"NoFT/dprRAG", "filename": f"{base_path}/{dataset_name}_costomized/results/{dataset_name}_{model_name}_bf_rag_dpr_full_results.jsonl"},
+        {"title": f"NoFT/bm25RAG", "filename": f"{base_path}/{dataset_name}_costomized/results/{model_type}/{dataset_name}_{model_name}_bf_rag_bm25_full_results.jsonl"},
+        {"title": f"NoFT/dprRAG", "filename": f"{base_path}/{dataset_name}_costomized/results/{model_type}/{dataset_name}_{model_name}_bf_rag_dpr_full_results.jsonl"},
         # {"title": f"FT/dprRAG", "filename": f"{base_path}/{dataset_name}_costomized/results/{dataset_name}_{model_name}_af_rag_dpr_peft_results.jsonl"},
     ]
     
@@ -632,12 +633,12 @@ def main():
     
     # == 2) Plot Retrival models output: Pre-relation & Pre-buckets
     # plot_retriever_results_per_relation()
-    plot_retriever_results_per_buckets(only_all=True)
+    # plot_retriever_results_per_buckets(only_all=True)
     
     # == 3) Plot QA models output
     # plot_answer_generator_results(per_relation=True, per_bucket=False, only_all=False)
     # plot_answer_generator_results(per_relation=False, per_bucket=True, only_all=False)
-    # plot_answer_generator_results(per_relation=False, per_bucket=False, only_all=True)
+    plot_answer_generator_results(per_relation=False, per_bucket=False, only_all=True)
     
     # == 4) Significance test
     
