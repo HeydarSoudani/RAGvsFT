a706c1fc1831242b1b15638c5c67cfe7bb50faed
HeydarSoudani <heydar.soudani@gmail.com>
2024-05-04 11:47:11 +0200

    some changes small

--- scripts/c3_multigpus_test.sh
+++ scripts/c3_multigpus_test.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --gpus=2
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
 #SBATCH --time=0:10:00
 #SBATCH --output=script_logging/slurm_%A.out
 
 module load 2022
+module load foss/2022a
 module load Python/3.10.4-GCCcore-11.3.0
 
 srun $HOME/RAGvsFT/component3_moe/multiple_gpus_test.py
+
+for i in `seq 1 $SLURM_NTASKS`; do
+  srun $HOME/RAGvsFT/component3_moe/multiple_gpus_test.py --index $i &
+done
+wait
 
 
 
@@ -19,6 +25,6 @@ srun $HOME/RAGvsFT/component3_moe/multiple_gpus_test.py
 #     "flant5_sm", "flant5_bs", "flant5_lg", "flant5_xl", "flant5_xxl",
 #     "stable_lm2", "tiny_llama", "MiniCPM",
 #     "llama2", "mistral", "zephyr"
-# ]
+# ] #SBATCH --output=script_logging/slurm_%A.out
 # retrieval method: ['ideal', 'dpr', 'contriever', 'rerank', 'bm25']
 
