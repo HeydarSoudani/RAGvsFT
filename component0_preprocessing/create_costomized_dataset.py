8e189b502fd1154ce9a47887e537f3e26e74d54b
HeydarS <heydar.soudani@gmail.com>
2024-05-21 01:39:47 +0200

    llama3 multi gpus added

--- component0_preprocessing/create_costomized_dataset.py
+++ component0_preprocessing/create_costomized_dataset.py
@@ -1033,7 +1033,8 @@ def create_ensamble_train_and_dev_files_prompting_llama3(relation_id):
     # Multiple GPUs
     accelerator = Accelerator()
     
-    model_name = "HuggingFaceH4/zephyr-7b-beta"
+    model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+    # model_name = "HuggingFaceH4/zephyr-7b-beta"
     # model_name = "meta-llama/Meta-Llama-3-8B"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
