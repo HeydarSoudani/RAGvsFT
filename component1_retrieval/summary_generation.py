599afc70479b56e25a7305fde50531afcc9194cf
HeydarS <heydar.soudani@gmail.com>
2024-05-24 15:30:56 +0200

    some files

--- component1_retrieval/summary_generation.py
+++ component1_retrieval/summary_generation.py
@@ -1,6 +1,97 @@
 #!/usr/bin/env python3
 
+import os
+import json
+import torch
+import random
+import logging
 import argparse
+import numpy as np
+from transformers import pipeline
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    handlers=[logging.StreamHandler()]
+)
+os.environ["WANDB_MODE"] = "offline"
+
+target_relation_ids = 'all'
+subset_percentage = 0.1
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def load_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def load_relations_data(args):
+    subfolders = ['test']
+        
+    relation_files = {}
+    for subfolder in subfolders:
+        subfolder_path = f"{args.data_dir}/{subfolder}"
+        if os.path.exists(subfolder_path):
+            for file in os.listdir(subfolder_path):
+                relation_id = file.split('.')[0]
+                if relation_id not in relation_files:
+                    relation_files[relation_id] = []
+                relation_files[relation_id].append(os.path.join(subfolder_path, file))    
+
+    # Select relations =================
+    if target_relation_ids == "all":
+        if args.dataset_name == 'popQA':
+            test_relation_ids = ['22', '91', '97', '106', '164', '182', '218', '257', '292', '422', '472', '484', '526', '533', '560', '639']
+        elif args.dataset_name == 'witQA':
+            test_relation_ids = ['17', '19', '22', '25', '27', '36', '50', '57', '58', '69', '86', '106', '123', '136', '140', '149', '162', '184', '344', '452', '462', '641', '674', '1038', '1050', '1376', '1431', '1433', '2012', '2936', '3301', '4647']
+        elif args.dataset_name == 'EQ':
+            test_relation_ids = ['17', '19', '20', '26', '30', '36', '40', '50', '69', '106', '112', '127', '131', '136', '159', '170', '175', '176', '264', '276', '407', '413', '495', '740', '800']
+    else:
+        test_relation_ids = target_relation_ids
+    
+    test_files = {subfolder: [] for subfolder in subfolders}
+    
+    for subfolder in subfolders:
+        subfolder_path = f"{args.data_dir}/{subfolder}"
+        if os.path.exists(subfolder_path):
+            for file in os.listdir(subfolder_path):
+                file_id = file.split('.')[0]
+                if file_id in test_relation_ids:
+                    test_files[subfolder].append(os.path.join(subfolder_path, file))
+
+    print("Selected Relation ID:", test_relation_ids)
+    logging.info(f"Selected Relation ID: {test_relation_ids}")
+
+    return test_relation_ids, test_files, relation_files
+
+def load_dataset(test_files):
+    test_data = []
+    for file in test_files['test']:
+        relation_id = file.split('/')[-1].split('.')[0]
+        data = load_json_file(file)
+        for item in data:
+            item['relation_id'] = relation_id
+        test_data.extend(data) 
+        
+    test_subset_size = int(subset_percentage * len(test_data))
+    subset_test_data = random.sample(test_data, test_subset_size)    
+    test_questions = [(item['query_id'], item['question'], item['pageviews'], item['relation_id']) for item in subset_test_data]
+    test_answers = [item['answers'] for item in subset_test_data]
+    
+    logging.info("Test dataset is loaded.")
+    # print("Test dataset is loaded.")
+    return test_questions, test_answers
+
+
 
 def summary_generation_for_retrieved_context(args):
     pipe = pipeline(
@@ -10,9 +101,12 @@ def summary_generation_for_retrieved_context(args):
         device_map="auto"
     )
     
+    test_relation_ids, test_files, relation_files = load_relations_data(args)
+    test_questions, test_answers = load_dataset(test_files)
+    
     # === Retrieved context ===
     ret_results = {}
-    ret_results_dir = f"{args.data_dir}/retrieved/{args.retrieval_method}"
+    ret_results_dir = f"{args.data_dir}/retrieved/{args.retrieval_method}_3"
     
     for test_relation_id in test_relation_ids:
         ret_results_path = f"{ret_results_dir}/{test_relation_id}.{args.retrieval_method}.ret_results.jsonl"
@@ -20,7 +114,6 @@ def summary_generation_for_retrieved_context(args):
             for line in file:
                 data = json.loads(line.strip())
                 ret_results[data['id']] = data
-    
     
 
 
