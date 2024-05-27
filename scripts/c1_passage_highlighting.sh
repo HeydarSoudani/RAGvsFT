169a8118aeb666ec0729c65120d9ba3f4629c827
HeydarSoudani <heydar.soudani@gmail.com>
2024-05-27 09:12:35 +0200

    file added

---
+++ scripts/c1_passage_highlighting.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=18
+#SBATCH --partition=gpu
+#SBATCH --time=1:00:00
+#SBATCH --output=script_logging/slurm_%A.out
+
+# Loading modules
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+
+srun $HOME/RAGvsFT/component1_retrieval/passage_highlighting.py \
+    --num_retrieved_passages 1 \
+    --dataset_name 'popQA' \
+    --retrieval_method "ideal" \
+    --seed 42
+
+
