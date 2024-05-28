9ff570775bb605e035ccbd94591f7546af00afa3
HeydarS <heydar.soudani@gmail.com>
2024-05-28 15:32:46 +0200

    highlight added

--- component2_AnsGen/evaluation/llm_evaluation.py
+++ component2_AnsGen/evaluation/llm_evaluation.py
@@ -278,7 +278,7 @@ def main(args):
     # == Loading highligted passages =========================
     qa_list = []
     highlight_results = {}
-    highlight_results_file = f'{args.data_dir}/retrieved_highlight/out_all.jsonl'
+    highlight_results_file = f'{args.data_dir}/retrieved_highlight/all_out.jsonl'
     with open (highlight_results_file, 'r') as file:
         for line in file:
             data = json.loads(line.strip())
