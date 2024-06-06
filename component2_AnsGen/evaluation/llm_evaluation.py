d9366260cf4acdaef73b546323ec04936b21ba63
HeydarS <heydar.soudani@gmail.com>
2024-06-06 23:40:16 +0200

    files added

--- component2_AnsGen/evaluation/llm_evaluation.py
+++ component2_AnsGen/evaluation/llm_evaluation.py
@@ -311,7 +311,7 @@ def main(args):
     if args.with_rag_sentence_rerank:
         ret_sent_rerank = {}
         # ret_results_dir = f"{args.data_dir}/reranked_sentences/{args.retrieval_method}_5"
-        ret_results_dir = f"{args.data_dir}/reranked_sentences/ideal_1_word_10"
+        ret_results_dir = f"{args.data_dir}/reranked_sentences/ideal_1_word_20"
         for test_relation_id in test_relation_ids:
             ret_results_path = f"{ret_results_dir}/{test_relation_id}.{args.retrieval_method}.set_reranked.jsonl"
             with open (ret_results_path, 'r') as file:
