aa79aa16bffb3d5b614ad21d27d6fdbf60e0dbc2
HeydarSoudani <heydar.soudani@gmail.com>
2024-06-11 13:26:02 +0200

    some files added

--- scripts/c2_llm_evaluation.sh
+++ scripts/c2_llm_evaluation.sh
@@ -13,8 +13,8 @@ module load Python/3.10.4-GCCcore-11.3.0
 
 # pip install git+https://github.com/huggingface/transformers
 srun $HOME/RAGvsFT/component2_AnsGen/evaluation/llm_evaluation.py \
-    --model_name_or_path "stabilityai/stablelm-2-zephyr-1_6b" \
-    --llm_model_name "stable_lm2" \
+    --model_name_or_path "google/flan-t5-base" \
+    --llm_model_name "flant5" \
     --dataset_name "popQA" \
     --with_peft False \
     --retrieval_method "dpr" \
@@ -27,7 +27,7 @@ srun $HOME/RAGvsFT/component2_AnsGen/evaluation/llm_evaluation.py \
     --with_rag_sentence_highlight False \
     --with_rag_qa_pairs False \
     --with_fewshot_examples False \
-    --output_file_pre_prefix "1rp_3p_bf" \
+    --output_file_pre_prefix "base_1rp_3p_bf" \
     --seed 42
 
 
