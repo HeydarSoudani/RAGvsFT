9e49e7a47c65bee3d7fa42199a6f123ab4e532ad
HeydarSoudani <heydar.soudani@gmail.com>
2024-06-22 01:02:44 +0200

    small changes

--- scripts/c2_flant5_finetuning.sh
+++ scripts/c2_flant5_finetuning.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=9
-#SBATCH --partition=gpu_mig
-#SBATCH --time=5:00:00
+#SBATCH --cpus-per-task=18
+#SBATCH --partition=gpu
+#SBATCH --time=4:00:00
 #SBATCH --output=script_logging/slurm_%A.out
 
 # Loading modules
@@ -12,15 +12,15 @@ module load 2022
 module load Python/3.10.4-GCCcore-11.3.0
 
 srun $HOME/RAGvsFT/component2_AnsGen/finetuning/flant5_finetuning.py \
-    --model_name_or_path "google/flan-t5-base" \
-    --llm_model_name "flant5_bs" \
+    --model_name_or_path "google/flan-t5-small" \
+    --llm_model_name "flant5_sm" \
     --dataset_name "EQ" \
     --generation_method "pipeline" \
     --epochs 5 \
     --batch_size 64 \
     --lr 0.0002 \
-    --with_peft True \
-    --version 111
+    --with_peft False \
+    --version 109
 
 # Model name: [
     # google/flan-t5-small
